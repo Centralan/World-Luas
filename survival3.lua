9edab0bdd51cc3e8713fca8240b95fbb4427952a
Centralan <Jessegauthier070296@gmail.com>
2023-09-12 17:31:07 -0400

    New Cheeves

--- survival3.lua
+++ survival3.lua
@@ -2,8 +2,19 @@ local myWorld = World:new('survival3');
 
 function lobby_easter_room(data)
          function lobby_easter_room(data)
+          local p = Player:new(data["player"]);
           p:sendEvent("achievement.timecapsule");
          end 
 end
 
 registerHook("REGION_ENTER", "lobby_easter_room", "survival3-spawn_easter");
+
+
+function survival_smarts(data)
+         function lobby_easter_room(data)
+         local p = Player:new(data["player"]);       
+          p:sendEvent("achievement.survivalsmarts");
+         end 
+end
+
+registerHook("INTERACT", "survival_smarts", 143, "survival3", -612.0, 64.0, -80.0);  
