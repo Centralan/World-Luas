e838b8e5aec6d437d64a310441b82e938a9ab355
Centralan <Jessegauthier070296@gmail.com>
2023-09-11 20:28:13 -0400

    added end

--- survival3.lua
+++ survival3.lua
@@ -3,6 +3,7 @@ local myWorld = World:new('survival3');
 function lobby_easter_room(data)
          function lobby_easter_room(data)
           p:sendEvent("achievement.timecapsule");
+         end 
 end
 
 registerHook("REGION_ENTER", "lobby_easter_room", "survival3-spawn_easter");
