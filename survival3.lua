47b0feeb4c23d40d3ba6191756dc9308e9ae7403
Centralan <Jessegauthier070296@gmail.com>
2023-09-24 20:40:51 -0400

    Update survival3.lua

--- survival3.lua
+++ survival3.lua
@@ -1,3 +1,7 @@
+------------------------------------
+------------spawn-------------------
+------------------------------------
+
 local myWorld = World:new('survival3');
 
 function lobby_easter_room(data)
@@ -15,11 +19,13 @@ function survival_smarts(data)
 
 registerHook("INTERACT", "survival_smarts", 143, "survival3", -612.0, 64.0, -80.0); 
 
+----------------------------------
+---------max----------------------
+----------------------------------
+local maxtp = Location:new(myWorld, -694.0, 169.0, -227.0);
 
+function maxtp(data)
+        local player = Player:new(data.player);
+          player:teleport(maxtp);
 
-function tramp_max(data)
-	local player = Player:new(data.player);
-	player:setVelocity(0, 3, 0);
-end
-
-registerHook("REGION_ENTER", "tramp_max", "survival3-tramp_max");
+registerHook("REGION_ENTER", "maxtp", "survival3-max_tp");
