d89cbb41a08a2678d0521e4fc72d67e7eb7b461d
Centralan <Jessegauthier070296@gmail.com>
2023-09-25 23:23:20 -0400

    MOVED Cat AI global lines to AI.LUA

--- creative.lua
+++ creative.lua
@@ -37,6 +37,7 @@ function cat_day(data)
 		local playerName = splitPlayerName(message, 16);
 		player:sendMessage("§aIt's now daytime!");
 	        c_broadcast(mut4, "§6" .. player.name .. " has purchased daytime. Praise the sun!");
+		creativesound:playSound('CAT_PURREOW', 100, 1);
 		myworld4:setTime(0);
 	end
 end
@@ -48,60 +49,10 @@ function cat_night(data)
 		local playerName = splitPlayerName(message, 16);
 		player:sendMessage("§aIt's now nighttime!");
 	        c_broadcast(mut5, "§6" .. player.name .. " has purchased daytime. Praise the sun!");
+		creativesound:playSound('CAT_PURREOW', 100, 1);
 		myworld4:setTime(14000);
 	end
 end
 
-
-function cat_meow(data)
-         local player = Player:new(data.player);
-                local message = data.message;
-                if hasPrefix(message, "cat hi") then
-                local playerName = splitPlayerName(message, 16);
-                c_broadcast(mut6, "§6" .. player.name .. " has purchased daytime. Praise the sun!");
-                c_broadcast2(mut6, "§6" .. player.name .. " has purchased daytime. Praise the sun!");
-                c_broadcast3(mut6, "§6" .. player.name .. " has purchased daytime. Praise the sun!");
-                creativesound:playSound('CAT_MEOW', 100, 1);
-                survivalsound:playSound('CAT_MEOW', 100, 1);
-
-        end
-end
-
-function cat_meow2(data)
-         local player = Player:new(data.player);
-                local message = data.message;
-                if hasPrefix(message, "hi cat") then
-                local playerName = splitPlayerName(message, 16);
-                c_broadcast(mut6, "§6" .. player.name .. " has purchased daytime. Praise the sun!");
-                c_broadcast2(mut6, "§6" .. player.name .. " has purchased daytime. Praise the sun!");
-                c_broadcast3(mut6, "§6" .. player.name .. " has purchased daytime. Praise the sun!");
-                creativesound:playSound('CAT_MEOW', 100, 1);
-                survivalsound:playSound('CAT_MEOW', 100, 1);
-
-        end
-end
-
-function cat_purr(data)
-         local player = Player:new(data.player);
-                local message = data.message;
-                if hasPrefix(message, "pets cat") then
-                local playerName = splitPlayerName(message, 16);
-                c_broadcast(mut7, "§6" .. player.name .. " has purchased daytime. Praise the sun!");
-                c_broadcast2(mut7, "§6" .. player.name .. " has purchased daytime. Praise the sun!");
-                c_broadcast3(mut7, "§6" .. player.name .. " has purchased daytime. Praise the sun!");
-                creativesound:playSound('CAT_PURR', 100, 1);
-                survivalsound:playSound('CAT_PURR', 100, 1);
-
-        end
-end
-
-registerHook("CHAT_MESSAGE", "cat_purr", "creative");
-registerHook("CHAT_MESSAGE", "cat_purr", "survival3");
 registerHook("CHAT_MESSAGE", "cat_day", "creative");
 registerHook("CHAT_MESSAGE", "cat_night", "creative");
-registerHook("CHAT_MESSAGE", "cat_meow", "creative");
-registerHook("CHAT_MESSAGE", "cat_meow2", "creative");
-registerHook("CHAT_MESSAGE", "cat_meow", "survival3");
-registerHook("CHAT_MESSAGE", "cat_meow2", "survival3");
-
-
