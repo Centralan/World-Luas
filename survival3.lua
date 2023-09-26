671817fb2a051983361ca3f74af4ef47c7d872a5
Centralan <Jessegauthier070296@gmail.com>
2023-09-25 20:17:52 -0400

    Added time shop

--- survival3.lua
+++ survival3.lua
@@ -35,6 +35,43 @@ registerHook("REGION_ENTER", "tramp", "survival3-tramp3");
 registerHook("REGION_ENTER", "tramp", "survival3-tramp4");
 registerHook("REGION_ENTER", "tramp2", "survival3-tramp5");
 
+-----------------------------
+---------time control----
+-----------------------------
+
+local totimeshop = Location:new(myWorld, -609.0, 56.0, -77.0);
+local exittimeshop = Location:new(myWorld, -639.0, 61.0, -65.0);
+
+function daytimeButtonClick(data)
+	local player = Player:new(data.player);
+		player:sendMessage("§aIt's now daytime!");
+		myworld:broadcast("§6" .. player.name .. " has purchased daytime. Praise the sun!");
+		myworld:setTime(600);
+end
+
+function nighttimeButtonClick(data)
+	local player = Player:new(data.player);
+		player:sendMessage("§aIt's now nighttime!");
+		myworld:broadcast("§6" .. player.name .. " has purchased night time. Praise the moon!");
+		myworld:setTime(1800);
+end
+
+function time_enter(data)
+        local targetplayer = Player:new(data.player);
+          targetplayer:teleport(totimeshop);
+end
+
+function time_exit(data)
+        local targetplayer = Player:new(data.player);
+          targetplayer:teleport(exittimeshop);
+end
+
+
+registerHook("INTERACT", "daytimeButtonClick", 143, "survival3", -607.0, 56.0, -79.0); 
+registerHook("INTERACT", "nighttimeButtonClick", 143, "survival3", -607.0, 56.0, -77.0); 
+registerHook("REGION_ENTER", "time_enter", "survival3-timeshop1");
+registerHook("REGION_ENTER", "time_exit", "survival3-timeshop2");
+
 ----------------------------------
 ---------max----------------------
 ----------------------------------
