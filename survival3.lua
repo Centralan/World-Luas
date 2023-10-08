2dcd3e59d56f8f0fe8a69f571b0f5bc7144964f4
Centralan <Jessegauthier070296@gmail.com>
2023-10-07 22:11:25 -0400

    gear cooldown

--- survival3.lua
+++ survival3.lua
@@ -20,8 +20,144 @@ end
 function s3_whisper_npc(npc, msg, player)
 	player:sendMessage('&f&c' .. npc .. '&f' .. msg);
 end
+
 ------------------------------------
-------------spawn-------------------
+-----------Spawn Gear-------------------
+------------------------------------
+local ig1 = Location:new(world, -611.0, 61.0, -71.0);
+local ig2 = Location:new(world, -611.0, 61.0, -69.0);
+local ig3 = Location:new(world, -611.0, 61.0, -67.0);
+local ig4 = Location:new(world, -611.0, 61.0, -65.0);
+local ig5 = Location:new(world, -611.0, 61.0, -63.0);
+
+local ig1ChestPlayers = {};
+local ig1ChestResetTimer = Timer:new("ig1_reset_chest", 200 * 200 * 500);
+local ig1ChestResetTimerRunning = false;
+local ig1ChestOpen = Location:new(world, -611.0, 61.0, -71.0);
+
+local ig2ChestPlayers = {};
+local ig2ChestResetTimer = Timer:new("ig2_reset_chest", 200 * 200 * 500);
+local ig2ChestResetTimerRunning = false;
+local ig2ChestOpen = Location:new(world, -611.0, 61.0, -69.0);
+
+local ig3ChestPlayers = {};
+local ig3ChestResetTimer = Timer:new("ig3_reset_chest", 200 * 200 * 500);
+local ig3ChestResetTimerRunning = false;
+local ig3ChestOpen = Location:new(world, -611.0, 61.0, -67.0);
+
+local ig4ChestPlayers = {};
+local ig4ChestResetTimer = Timer:new("ig2_reset_chest", 200 * 200 * 500);
+local ig4ChestResetTimerRunning = false;
+local ig4ChestOpen = Location:new(world, -611.0, 61.0, -65.0);
+
+local ig5ChestPlayers = {};
+local ig5ChestResetTimer = Timer:new("ig2_reset_chest", 200 * 200 * 500);
+local ig5ChestResetTimerRunning = false;
+local ig5ChestOpen = Location:new(world, -611.0, 61.0, -63.0);
+
+function ig1(data)
+        local player = Player:new(data.player);
+        if not ig1ChestPlayers[player.name] then
+                ig1:cloneChestToPlayer(player.name);
+                player:closeInventory();
+                ig1ChestPlayers[player.name] = true;
+
+                if not ig1ChestResetTimerRunning then
+                       ig1ChestResetTimerRunning = true;
+                        ig1ChestResetTimer:start();
+                end
+        end
+end
+
+function ig1_reset_chest()
+	ig1ChestPlayers = {};
+	ig1ChestResetTimerRunning = false;
+end
+
+function ig2(data)
+        local player = Player:new(data.player);
+        if not ig2ChestPlayers[player.name] then
+                ig2:cloneChestToPlayer(player.name);
+                player:closeInventory();
+                ig2ChestPlayers[player.name] = true;
+
+                if not ig2ChestResetTimerRunning then
+                       ig2ChestResetTimerRunning = true;
+                        ig2ChestResetTimer:start();
+                end
+        end
+end
+
+function ig2_reset_chest()
+	ig2ChestPlayers = {};
+	ig2ChestResetTimerRunning = false;
+end
+
+function ig3(data)
+        local player = Player:new(data.player);
+        if not ig3ChestPlayers[player.name] then
+                ig3:cloneChestToPlayer(player.name);
+                player:closeInventory();
+                ig3ChestPlayers[player.name] = true;
+
+                if not ig3ChestResetTimerRunning then
+                       ig3ChestResetTimerRunning = true;
+                        ig3ChestResetTimer:start();
+                end
+        end
+end
+
+function ig3_reset_chest()
+	ig3ChestPlayers = {};
+	ig3ChestResetTimerRunning = false;
+end
+
+function ig4(data)
+        local player = Player:new(data.player);
+        if not ig4ChestPlayers[player.name] then
+                ig4:cloneChestToPlayer(player.name);
+                player:closeInventory();
+                ig4ChestPlayers[player.name] = true;
+
+                if not ig4ChestResetTimerRunning then
+                       ig4ChestResetTimerRunning = true;
+                        ig4ChestResetTimer:start();
+                end
+        end
+end
+
+function ig4_reset_chest()
+	ig4ChestPlayers = {};
+	ig4ChestResetTimerRunning = false;
+end
+
+function ig5(data)
+        local player = Player:new(data.player);
+        if not ig5ChestPlayers[player.name] then
+                ig5:cloneChestToPlayer(player.name);
+                player:closeInventory();
+                ig5ChestPlayers[player.name] = true;
+
+                if not ig5ChestResetTimerRunning then
+                       ig5ChestResetTimerRunning = true;
+                        ig5ChestResetTimer:start();
+                end
+        end
+end
+
+function ig5_reset_chest()
+	ig5ChestPlayers = {};
+	ig5ChestResetTimerRunning = false;
+end
+
+registerHook("INTERACT", "ig1", 143, "survival3", -612.0, 63.0, -68.0); 
+registerHook("INTERACT", "ig2", 143, "survival3", -612.0, 63.0, -67.0); 
+registerHook("INTERACT", "ig3", 143, "survival3", -612.0, 63.0, -66.0); 
+registerHook("INTERACT", "ig4", 143, "survival3", -612.0, 63.0, -65.0); 
+registerHook("INTERACT", "ig5", 143, "survival3", -612.0, 63.0, -64.0); 
+
+------------------------------------
+-----------Cheeves-------------------
 ------------------------------------
 
 function lobby_easter_room(data)
@@ -40,20 +176,8 @@ function survival_smarts(data)
 
 registerHook("INTERACT", "survival_smarts", 143, "survival3", -612.0, 64.0, -80.0); 
 
-function tramp(data)
-	local player = Player:new(data.player);
-	player:setVelocity(0, 3, 0);
-end
 
-function tramp2(data)
-	local player = Player:new(data.player);
-	player:setVelocity(0, 10, 0);
-end
 
-registerHook("REGION_ENTER", "tramp", "survival3-tramp1");
-registerHook("REGION_ENTER", "tramp", "survival3-tramp2");
-registerHook("REGION_ENTER", "tramp", "survival3-tramp3");
-registerHook("REGION_ENTER", "tramp", "survival3-tramp4");
 registerHook("REGION_ENTER", "tramp2", "survival3-tramp5");
 
 
