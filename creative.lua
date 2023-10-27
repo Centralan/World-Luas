a7501453c49e86363fac204d3341e910e5405703
Centralan <Jessegauthier070296@gmail.com>
2023-10-26 21:13:25 -0400

    Creative.lua - PKR failure tp event

--- creative.lua
+++ creative.lua
@@ -66,4 +66,33 @@ end
 
 registerHook("REGION_ENTER", "pkr_mode", "creative-centralan_7");
 
+--------pkr_desert--------
+
+local pkr_d = Location:new(myWorld4, -1393.531, 71.0, 5653.0);
+pkr_d:setYaw(-90.4);
+pkr_d:setPitch(6.6);
+
+function pkr_d_respawn(data)
+	local player = Player:new(data["player"]);
+	player:teleport(pkr_d);
+end
+
+registerHook("REGION_ENTER", "pkr_d_respawn", "creative-pkr_d_1");
+registerHook("REGION_ENTER", "pkr_d_respawn", "creative-pkr_d_2");
+
+--------pkr_plains--------
+
+local pkr_p = Location:new(myWorld4, -1407.279, 71.0, 5666.0);
+pkr_p:setYaw(1.2);
+pkr_p:setPitch(3.6);
+
+function pkr_p_respawn(data)
+	local player = Player:new(data["player"]);
+	player:teleport(pkr_p);
+end
+
+registerHook("REGION_ENTER", "pkr_p_respawn", "creative-pkr_p_1");
+registerHook("REGION_ENTER", "pkr_p_respawn", "creative-pkr_p_2");
+registerHook("REGION_ENTER", "pkr_p_respawn", "creative-pkr_p_3");
+registerHook("REGION_ENTER", "pkr_p_respawn", "creative-pkr_p_4");
 
