1a6c7b3779670c055f2338e78b4213865391b82e
Centralan <Jessegauthier070296@gmail.com>
2024-01-01 15:54:35 -0500

    Update Cleanup.lua - Krimas is over

--- Cleanup.lua
+++ Cleanup.lua
@@ -2,15 +2,68 @@ local world = World:new('survival3');
 
 function event_cleanup(data)
   local player = Player:new(data.player);
+local world = World:new('survival3');
+
+function event_cleanup(data)
+  local player = Player:new(data.player);
    player:removePermission("runsafe.warp.use.grave1");
    player:removePermission("runsafe.warp.use.grave2");
    player:removePermission("runsafe.warp.use.grave3");
    player:removePermission("runsafe.warp.use.grave4");
    player:removePermission("runsafe.warp.use.grave5");
    player:removePermission("runsafe.warp.use.grave6");
+   player:removePermission("runsafe.event.trees");
+   player:removePermission("runsafe.event.emerald");
+   player:removePermission("runsafe.event.flower");
+   player:removePermission("runsafe.event.dog");
+   player:removePermission("runsafe.event.city");
+   player:removePermission("runsafe.event.sky");
+   player:removePermission("runsafe.event.skally");
+   player:removePermission("runsafe.event.az");
+   player:removePermission("runsafe.event.beacon");
+   player:removePermission("runsafe.event.feather");
+   player:removePermission("runsafe.event.igloo");
+   player:removePermission("runsafe.warp.use.krimas");
+   player:removePermission("runsafe.event.gift1");
+   player:removePermission("runsafe.event.gift2");
+   player:removePermission("runsafe.event.gift3");
+   player:removePermission("runsafe.event.gift4");
+   player:removePermission("runsafe.event.gift5");
+   player:removePermission("runsafe.event.gift6");
+   player:removePermission("runsafe.event.gift7");
+   player:removePermission("runsafe.event.gift8");
+   player:removePermission("runsafe.event.gift9");
+   player:removePermission("runsafe.event.gift10");
+   player:removePermission("runsafe.event.gift11");
+   player:removePermission("runsafe.event.gift12");
+   player:removePermission("runsafe.event.gift13");
+   player:removePermission("runsafe.event.gift14");
+   player:removePermission("runsafe.event.gift15");
+   player:removePermission("runsafe.event.gift16");
+   player:removePermission("runsafe.event.gift17");
+   player:removePermission("runsafe.event.gift18");
+   player:removePermission("runsafe.event.lighthouse");
+   player:removePermission("runsafe.event.vt");
+   player:removePermission("runsafe.event.enchant");
+   player:removePermission("runsafe.event.boat");
+   player:removePermission("runsafe.event.weep");
+   player:removePermission("runsafe.event.weepd");
+   player:removePermission("runsafe.event.break");
+   player:removePermission("runsafe.event.pray");
+   player:removePermission("runsafe.event.1");
+   player:removePermission("runsafe.event.2");
+   player:removePermission("runsafe.event.3");
+   player:removePermission("runsafe.event.4");
+   player:removePermission("runsafe.event.5");
+   player:removePermission("runsafe.event.6");
+   player:removePermission("runsafe.event.7");
+   player:removePermission("runsafe.event.8");
+   player:removePermission("runsafe.event.9");
+   player:removePermission("runsafe.event.10");
+
 end
 
-registerHook("REGION_ENTER", "event_cleanup", "survival3-spawn);
+registerHook("REGION_ENTER", "event_cleanup", "survival3-spawn");
 
 --Halloween 2023 Perms List
  --runsafe.warp.use.grave1
